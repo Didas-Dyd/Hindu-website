eed8971896fffcfc0627a9e7b6e3f4eaf7ae21cc
unknown <nshimiyimanadidas5@gmail.com>
2021-07-23 12:04:08 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,178 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HINDU</title>
+    <link rel="stylesheet"
+    href="./asset/css/styles.css">
+
+   
+
+</head>
+  <body>
+  <div class="header"> <h1 style="color: orange; text-align: center;">HINDU MANDAL OF RWANDA</h1>
+    <hr style="margin-left: 10%; margin-right: 10%;">
+    
+    <header>
+        <nav class="fixed">
+            <h1 style="text-align: center; color: rgb(122, 110, 98);">
+            <a href="./index.php">HOMEPAGE</a>
+            <a href="./component/events.html">EVENTS</a>
+            <a href="./component/about.html">ABOUT US</a>
+            <a href="./component/support.html">SUPPORT US</a>
+            <a href="./component/contact.html">CONTACT US</a>
+            </h1><br>  
+        </nav> 
+     </header>
+     </div>
+     <div class="upcoming">
+        <section>UPCOMING EVENTS <hr class="line">
+        <div class="upcoming-item">
+            <p>
+               
+                HINDU TEMPLE OF RWANDA'S SUMMIT
+            </p>
+        
+        <p class="months">17 April 2021</p>
+        <p class="months">kicukiro<p>
+    </div><hr class="line">
+    <div class="upcoming-item">
+            <p>
+               
+                HINDU TEMPLE OF RWANDA'S SUMMIT
+            </p>
+        
+        <p class="months">30 April 2021</p>
+        <p class="months">kicukiro<p>
+    </div><hr class="line">
+    <div class="upcoming-item">
+            <p>
+               
+                HINDU TEMPLE OF RWANDA'S SUMMIT
+            </p>
+        
+        <p class="months">30 May 2021</p>
+        <p class="months">kicukiro<p>
+    </div><hr class="line">
+    <div class="upcoming-item">
+            <p>
+               
+                HINDU TEMPLE OF RWANDA'S SUMMIT
+            </p>
+        
+        <p class="months">30 June 2021</p>
+        <p class="months">kicukiro<p>
+    </div><hr class="line">
+    <div class="upcoming-item">
+            <p>
+               
+                HINDU TEMPLE OF RWANDA'S SUMMIT
+            </p>
+        
+        <p class="months">30 July 2021</p>
+        <p class="months">kicukiro<p>
+    </div><hr class="line">
+    <div class="upcoming-item">
+            <p>
+               
+                HINDU TEMPLE OF RWANDA'S SUMMIT
+            </p>
+        
+        <p class="months">30 August 2021</p>
+        <p class="months">kicukiro<p>
+    </div><hr class="line">
+    <div class="upcoming-item">
+            <p>
+               
+                HINDU TEMPLE OF RWANDA'S SUMMIT
+            </p>
+        
+        <p class="months">30 August 2021</p>
+        <p class="months">kicukiro<p>
+    </div><hr class="line">
+
+     </section>
+     
+     
+     <section>
+         TEMPLE HOURS
+         <hr class="line">
+        <div class="upcoming-item">
+            <p>
+               
+                MONDAY
+            </p>
+        
+        <p class="months">17 April 2021</p>
+        <p class="months">kicukiro<p>
+    </div><hr class="line">
+    <div class="upcoming-item">
+            <p>
+               
+                TUESDAY
+            </p>
+        
+        <p class="months">30 April 2021</p>
+        <p class="months">kicukiro<p>
+    </div><hr class="line">
+    <div class="upcoming-item">
+            <p>
+               
+                WEDNESDAY
+            </p>
+        
+        <p class="months">30 May 2021</p>
+        <p class="months">kicukiro<p>
+    </div><hr class="line">
+    <div class="upcoming-item">
+            <p>
+               
+                THURSDAY
+            </p>
+        
+        <p class="months">30 June 2021</p>
+        <p class="months">kicukiro<p>
+    </div><hr class="line">
+    <div class="upcoming-item">
+            <p>
+               
+                FRIDAY
+            </p>
+        
+        <p class="months">30 July 2021</p>
+        <p class="months">kicukiro<p>
+    </div><hr class="line">
+    <div class="upcoming-item">
+            <p>
+               
+                SATURDAY
+            </p>
+        
+        <p class="months">30 August 2021</p>
+        <p class="months">kicukiro<p>
+    </div><hr class="line">
+    <div class="upcoming-item">
+            <p>
+               
+                SUNDAY
+            </p>
+        
+        <p class="months">30 August 2021</p>
+        <p class="months">kicukiro<p>
+    </div><hr class="line">
+     </section>
+     
+     
+</div>
+<button style="margin-left: 15%">
+         SEE ALL EVENTS
+     </button>
+<footer class="footer">
+    <div class="footer-holder">
+         Copyright 2021 HINDU TEMPLE OF RWANDA
+   </div>
+</footer>
+  </body>
+</html>
